6cbddccf44688fb298c2c0047bd2bac84cab7ac0
Rajesh <rajarajesha@gmail.com>
2025-11-05 12:54:13 +0530

    'new'

---
+++ 18 Viewsets and Routers/food_delivery_api/food_delivery_api/urls.py
@@ -0,0 +1,16 @@
+from rest_framework.routers import DefaultRouter
+from django.contrib import admin
+from django.urls import path, include
+from delivery.views import RestaurantViewSet, MenuItemViewSet, CustomerViewSet, OrderViewSet
+
+router = DefaultRouter()
+
+router.register(r'restaurants', RestaurantViewSet)
+router.register(r'menu-items', MenuItemViewSet)
+router.register(r'customers', CustomerViewSet)
+router.register(r'orders', OrderViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+]
