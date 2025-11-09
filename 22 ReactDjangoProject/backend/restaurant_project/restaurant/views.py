ed24453c0272475521cb42c3135241d83433d93c
Rajesh <rajarajesha@gmail.com>
2025-11-09 22:51:36 +0530

    'reactdjngo'

---
+++ 22 ReactDjangoProject/backend/restaurant_project/restaurant/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import viewsets
+from .models import Restaurant
+from .serializers import RestaurantSerializer
+
+class RestaurantViewSet(viewsets.ModelViewSet):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
